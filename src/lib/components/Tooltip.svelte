53a1e8014f0e1df30eda248080fe676ce7a7060e
Gorgatron1 <david_koski@icloud.com>
2025-05-03 11:21:58 -0700

    add feature tooltips

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -11,13 +11,17 @@
 	};
 </script>
 
-<div use:popup={popupHover}>
+{#if content}
+	<div use:popup={popupHover}>
+		<slot />
+	</div>
+	<div
+		class="card px-2 py-1 variant-filled-surface z-50 border-primary-500 border shadow-md"
+		data-popup={id}
+	>
+		<p>{content}</p>
+		<div class="arrow variant-filled-surface border-primary-500 border-r border-b" />
+	</div>
+{:else}
 	<slot />
-</div>
-<div
-	class="card px-2 py-1 variant-filled-surface z-50 border-primary-500 border shadow-md"
-	data-popup={id}
->
-	<p>{content}</p>
-	<div class="arrow variant-filled-surface border-primary-500 border-r border-b" />
-</div>
+{/if}
