69ecfa0264f90a344e5cf3f02180991cddd46930
Gorgatron1 <david_koski@icloud.com>
2026-02-04 19:12:32 -0800

    handle null file.version

--- src/lib/components/cards/TableDetailCard.svelte
+++ src/lib/components/cards/TableDetailCard.svelte
@@ -22,7 +22,9 @@
 			<p class="font-bold max-h-48 overflow-hidden max-w-full">
 				{file.authors?.join(', ')}
 			</p>
+			{#if file.version}
 			<p>{file.version}</p>
+			{/if}
 			{#if file.comment}
 				<p>{file.comment}</p>
 			{/if}
